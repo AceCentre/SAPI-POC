06b85c1bcfb4929781865777cb15167fac592b23
will wade <willwade@gmail.com>
2024-09-14 15:57:47 +0100

    reverting engine.cpp

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -37,7 +37,7 @@ void Engine::FinalRelease()
     speak_method_.reset();
 }
 
-HRESULT __stdcall Engine::SetObjectToken(ISpObjectToken* pToken)
+HRESULT __stdcall Engine::SetObjectToken(ISpObjectToken *pToken)
 {
     slog("Engine::SetObjectToken");
 
@@ -46,40 +46,46 @@ HRESULT __stdcall Engine::SetObjectToken(ISpObjectToken* pToken)
 
     CSpDynamicString voice_name;
     hr = token_->GetStringValue(L"", &voice_name);
-    if (hr != S_OK) {
+    if (hr != S_OK)
+    {
         return hr;
     }
 
     CSpDynamicString path;
     hr = token_->GetStringValue(L"Path", &path);
-    if (hr != S_OK) {
+    if (hr != S_OK)
+    {
         return hr;
     }
 
     CSpDynamicString mod;
     hr = token_->GetStringValue(L"Module", &mod);
-    if (hr != S_OK) {
+    if (hr != S_OK)
+    {
         return hr;
     }
 
     CSpDynamicString cls;
     hr = token_->GetStringValue(L"Class", &cls);
-    if (hr != S_OK) {
+    if (hr != S_OK)
+    {
         return hr;
     }
 
-    slog(L"Path={}", (const wchar_t*)path);
-    slog(L"Module={}", (const wchar_t*)mod);
-    slog(L"Class={}", (const wchar_t*)cls);
+    slog(L"Path={}", (const wchar_t *)path);
+    slog(L"Module={}", (const wchar_t *)mod);
+    slog(L"Class={}", (const wchar_t *)cls);
 
     pycpp::ScopedGIL lock;
 
     // Append to sys.path
-    std::wstring_view path_view {(const wchar_t*)path, path.Length()};
+    std::wstring_view path_view{(const wchar_t *)path, path.Length()};
 
-    for (size_t offset = 0;;) {
+    for (size_t offset = 0;;)
+    {
         auto pos = path_view.find(L';', offset);
-        if (pos == std::wstring_view::npos) {
+        if (pos == std::wstring_view::npos)
+        {
             pycpp::append_to_syspath(path_view.substr(offset));
             break;
         }
@@ -87,139 +93,17 @@ HRESULT __stdcall Engine::SetObjectToken(ISpObjectToken* pToken)
         offset += pos + 1;
     }
 
-    auto mod_utf8 = utf8_encode((const wchar_t*)mod);
-    auto cls_utf8 = utf8_encode((const wchar_t*)cls);
+    auto mod_utf8 = utf8_encode((const wchar_t *)mod);
+    auto cls_utf8 = utf8_encode((const wchar_t *)cls);
 
     // Initialize voice
-    pycpp::Obj module {PyImport_ImportModule(mod_utf8.c_str())};
+    pycpp::Obj module{PyImport_ImportModule(mod_utf8.c_str())};
     pycpp::Obj dict(pycpp::incref(PyModule_GetDict(module)));
     pycpp::Obj voice_class(pycpp::incref(PyDict_GetItemString(dict, cls_utf8.c_str())));
-
-    // Call the Python `VoiceServerVoice` class (or whatever class is defined in Python)
     pycpp::Obj voice_object(PyObject_CallNoArgs(voice_class));
     speak_method_ = PyObject_GetAttrString(voice_object, "speak");
 
     return hr;
-}
-
-HRESULT __stdcall Engine::Speak(DWORD dwSpeakFlags, REFGUID rguidFormatId, const WAVEFORMATEX* pWaveFormatEx,
-                                const SPVTEXTFRAG* pTextFragList, ISpTTSEngineSite* pOutputSite)
-{
-    slog("Engine::Speak");
-
-    pycpp::ScopedGIL lock;
-
-    for (const auto* text_frag = pTextFragList; text_frag != nullptr; text_frag = text_frag->pNext) {
-        if (handle_actions(pOutputSite) == 1) {
-            return S_OK;
-        }
-
-        slog(L"action={}, offset={}, length={}, text=\"{}\"",
-            (int)text_frag->State.eAction,
-            text_frag->ulTextSrcOffset,
-            text_frag->ulTextLen, 
-            text_frag->pTextStart);
-
-        // Prepare the text to send to the pipe service
-        pycpp::Obj text {pycpp::convert({text_frag->pTextStart, text_frag->ulTextLen})};
-        
-        // Call the Python `speak` method via the pipe service
-        pycpp::Obj generator(PyObject_CallOneArg(speak_method_, text));
-        assert(PyIter_Check(generator));
-
-        PyObject* item;
-        pycpp::Obj obj;
-        Py_buffer view;
-        int flags = PyBUF_C_CONTIGUOUS | PyBUF_SIMPLE;
-
-        while ((item = PyIter_Next(generator))) {
-            obj = item;
-
-            assert(PyObject_CheckBuffer(obj));
-            if (PyObject_GetBuffer(obj, &view, flags) == -1) {
-                throw pycpp::PythonException("PyObject_GetBuffer failed");
-            }
-
-            assert(view.ndim == 1);
-
-            ULONG written;
-            HRESULT result = pOutputSite->Write(view.buf, view.len, &written);
-            assert(result == S_OK);
-            assert(view.len == written);
-
-            slog("Engine::Speak written={}", written);
-
-            PyBuffer_Release(&view);
-        }
-
-        slog("Engine::Speak end of fragment");
-
-        pycpp::throw_on_error();
-    }
-
-    return S_OK;
-}
-
-HRESULT __stdcall Engine::Speak(DWORD dwSpeakFlags, REFGUID rguidFormatId, const WAVEFORMATEX *pWaveFormatEx,
-                                const SPVTEXTFRAG *pTextFragList, ISpTTSEngineSite *pOutputSite)
-{
-    slog("Engine::Speak");
-
-    pycpp::ScopedGIL lock;
-
-    for (const auto *text_frag = pTextFragList; text_frag != nullptr; text_frag = text_frag->pNext)
-    {
-        if (handle_actions(pOutputSite) == 1)
-        {
-            return S_OK;
-        }
-
-        slog(L"action={}, offset={}, length={}, text=\"{}\"",
-             (int)text_frag->State.eAction,
-             text_frag->ulTextSrcOffset,
-             text_frag->ulTextLen,
-             text_frag->pTextStart);
-
-        // Prepare the text to send to the pipe service
-        pycpp::Obj text{pycpp::convert({text_frag->pTextStart, text_frag->ulTextLen})};
-
-        // Call the Python `speak` method via the pipe service
-        pycpp::Obj generator(PyObject_CallOneArg(speak_method_, text));
-        assert(PyIter_Check(generator));
-
-        PyObject *item;
-        pycpp::Obj obj;
-        Py_buffer view;
-        int flags = PyBUF_C_CONTIGUOUS | PyBUF_SIMPLE;
-
-        while ((item = PyIter_Next(generator)))
-        {
-            obj = item;
-
-            assert(PyObject_CheckBuffer(obj));
-            if (PyObject_GetBuffer(obj, &view, flags) == -1)
-            {
-                throw pycpp::PythonException("PyObject_GetBuffer failed");
-            }
-
-            assert(view.ndim == 1);
-
-            ULONG written;
-            HRESULT result = pOutputSite->Write(view.buf, view.len, &written);
-            assert(result == S_OK);
-            assert(view.len == written);
-
-            slog("Engine::Speak written={}", written);
-
-            PyBuffer_Release(&view);
-        }
-
-        slog("Engine::Speak end of fragment");
-
-        pycpp::throw_on_error();
-    }
-
-    return S_OK;
 }
 
 HRESULT __stdcall Engine::GetObjectToken(ISpObjectToken **ppToken)
